333dfd8cc1e1699812c307457474eea0aa23ef3c
Marcin Zamorski <marcin.zamorski@gmail.com>
2024-08-30 15:25:23 +0200

    Fix script to correctly change directories on different drives

--- restart-shell.bat
+++ restart-shell.bat
@@ -1,15 +1,26 @@
 @echo off
 
 
-set ProjectBinFolder=D:\__PROJEKTY\WùASNE\ArchiveNow\trunk\bin\Debug
+set ProjectBinFolder=E:\__PROJECTS\MY\ArchiveNow\ArchiveNow\bin\Debug
 set ProjectSrcFolder=%cd%
 rem set ProjectRootFolder=%ProjectSrcFolder%
 
-set AppFolder=D:\Program Files (x86)\ArchiveNow
-set RoamingAppFolder=C:\Users\Marcin\AppData\Roaming\ArchiveNow\Profiles
+set AppFolder=C:\Program Files (x86)\ArchiveNow
+set AppFolderDriveLetter=%AppFolder:~0,2%
+set RoamingAppFolder=C:\Users\marci\AppData\Roaming\ArchiveNow\Profiles
 set SrmPath="%AppFolder%\srm.exe"
 
-cd "%AppFolder%"
+
+if exist "%AppFolder%" (
+	%AppFolderDriveLetter%
+    cd "%AppFolder%"
+    echo Directory changed to: "%AppFolder%"
+) else (
+    echo The directory "%AppFolder%" does not exist.
+	exit /b
+)
+
+echo The current directory is: "%CD%"
 
 %SrmPath% uninstall ArchiveNow.Shell.dll && taskkill /F /IM explorer.exe && explorer.exe
 
