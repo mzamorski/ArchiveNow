b319f4b0a255796690d9359a469e6b981968d6e7
Marcin Zamorski <marcin.zamorski@gmail.com>
2024-09-19 21:43:51 +0200

    Change `ListingProvider` formatting.

--- src/ArchiveNow.Providers.Listing/ListingProvider.cs
+++ src/ArchiveNow.Providers.Listing/ListingProvider.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Security.Cryptography;
 
 using ArchiveNow.Providers.Core;
+using ArchiveNow.Providers.Core.FileNameBuilders.Formatters;
 
 namespace ArchiveNow.Providers.Listing
 {
@@ -164,11 +165,14 @@ namespace ArchiveNow.Providers.Listing
 
     internal class DefaultListingEntryFormatter : IListingEntryFormatter
     {
+        private const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ssZ";
+        private const string Separator = "|";
+
         public static DefaultListingEntryFormatter Instance { get; } = new Lazy<DefaultListingEntryFormatter>(() => new DefaultListingEntryFormatter()).Value;
 
         public string Format(ListingEntry entry)
         {
-            return $"{entry.Path}|{entry.Size}|{entry.ModifiedDate}|{entry.Hash}";
+            return $"{entry.Path}{Separator}{entry.Size}{Separator}{entry.ModifiedDate.ToString(DateTimeFormat)}{Separator}{entry.Hash}";
         }
     }
 }
