323a1d4559cf60f4b9fcd30b8a38dc87db2ce8b3
Marcin Zamorski <marcin.zamorski@gmail.com>
2025-11-22 21:09:33 +0100

    fix(notifications): skip toast execution when system notifications are disabled

--- src/ArchiveNow.Notifications.Core/ToastNotificatioService.cs
+++ src/ArchiveNow.Notifications.Core/ToastNotificatioService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
-using System;
-using System.IO;
+
 using Windows.Foundation.Collections;
 using Windows.UI.Notifications;
 
@@ -17,12 +16,25 @@ namespace ArchiveNow.Notifications.Core
 
                 // Obtain any user input (text boxes, menu selections) from the notification
                 ValueSet userInput = toastArgs.UserInput;
-
             };
         }
 
         public void Show(NotificationMessage msg)
         {
+            if (!AreNotificationsEnabledForApp(out var reason))
+            {
+                // TODO
+                //
+                // Windows Services cannot display UI elements such as MessageBox because they run in session 0
+                // without access to the interactive user desktop. Any attempt to show a dialog from a service
+                // will either fail silently or block the service. User-facing notifications must be triggered
+                // from the WPF application, not from the service process.
+
+                //MessageBox.Show(reason + " Please enable notifications in Windows settings.", "ArchiveNow", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+                return;
+            }
+
             var builder = new ToastContentBuilder()
                 //.AddAppLogoOverride(new Uri(iconFilePath))
                 .AddText(msg.Title)
@@ -42,6 +54,38 @@ namespace ArchiveNow.Notifications.Core
             ToastNotificationManagerCompat
                 .CreateToastNotifier()
                 .Show(notif);
+        }
+
+        private bool AreNotificationsEnabledForApp(out string reason)
+        {
+            var setting = ToastNotificationManagerCompat.CreateToastNotifier().Setting;
+
+            switch (setting)
+            {
+                case NotificationSetting.Enabled:
+                    reason = null;
+                    return true;
+
+                case NotificationSetting.DisabledForApplication:
+                    reason = "Notifications are disabled for this application in Windows settings.";
+                    return false;
+
+                case NotificationSetting.DisabledForUser:
+                    reason = "Notifications are disabled for the current user in Windows settings.";
+                    return false;
+
+                case NotificationSetting.DisabledByGroupPolicy:
+                    reason = "Notifications are disabled by system policy.";
+                    return false;
+
+                case NotificationSetting.DisabledByManifest:
+                    reason = "Notifications are disabled by app manifest.";
+                    return false;
+
+                default:
+                    reason = "Notifications are not available.";
+                    return false;
+            }
         }
     }
 }
