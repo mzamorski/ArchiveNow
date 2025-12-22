c246574c08ae3362a913c76f29314a5e7988751e
Marcin Zamorski <marcin.zamorski@gmail.com>
2025-12-22 19:44:29 +0100

    feat(providers): initial implementation of `Lz4ArchiveProvider` (WIP)

--- src/ArchiveNow.Providers.Lz4/Lz4ArchiveProvider.cs
+++ src/ArchiveNow.Providers.Lz4/Lz4ArchiveProvider.cs
@@ -1,44 +1,62 @@
 ﻿using System.IO;
+
 using ArchiveNow.Providers.Core;
 using ArchiveNow.Providers.Core.PasswordProviders;
 
+using K4os.Compression.LZ4.Streams;
 
 namespace ArchiveNow.Providers.Lz4
 {
     public class Lz4ArchiveProvider : ArchiveProviderBase
     {
-        public Lz4ArchiveProvider(IArchiveFilePathBuilder archiveFilePathBuilder, IPasswordProvider passwordProvider) 
-            : base(archiveFilePathBuilder, passwordProvider)
-        {
-            
-        }
+        private readonly Stream _archiveStream;
+        private LZ4EncoderStream _lz4Stream;
 
         public override string FileExtension => "lz4";
 
+        public Lz4ArchiveProvider(IArchiveFilePathBuilder archiveFilePathBuilder, IPasswordProvider passwordProvider)
+            : base(archiveFilePathBuilder, passwordProvider)
+        {
+            _archiveStream = File.Create(ArchiveFilePath);
+        }
+
         public override void AddDirectory(string path)
         {
-            foreach (var filePath in Directory.EnumerateFiles(path))
-            {
-                Add(filePath);
-            }
+            CompressDirectory(path);
         }
 
         public override void Add(string path)
         {
+            CompressFile(path);
 
+            OnFileCompressed();
         }
 
         public override void BeginUpdate(string sourcePath)
-        { }
+        {
+            // Create LZ4 compression stream
+            _lz4Stream = LZ4Stream.Encode(_archiveStream, leaveOpen: true);
+        }
 
         public override void CommitUpdate()
         {
+            CurrentProgressMode = ProgressMode.Indeterminate;
 
+            OnFinished();
         }
 
         public override void AbortUpdate()
-        {
+        { }
 
+        private void CompressDirectory(string directoryPath)
+        { }
+
+        private void CompressFile(string filePath)
+        {
+            using (var fileStream = File.OpenRead(filePath))
+            {
+                fileStream.CopyTo(_lz4Stream);
+            }
         }
     }
 }
